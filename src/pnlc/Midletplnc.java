f7be11a93aedac0692901aef12035c28b33cd808
fadiga <ibfadiga@gmail.com>
2013-11-12 16:55:52 +0000

    Création du projet sdk

---
+++ src/pnlc/Midletplnc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pnlc;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author FAD
+ */
+public class Midletplnc extends MIDlet {
+    public void startApp() {
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
